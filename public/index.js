8bb77242f9d78240ed6f8d4d4688ba820ba7be9b
int <64084503+chul0721@users.noreply.github.com>
2020-11-02 23:57:38 +0900

    v 1.0

--- public/index.js
+++ public/index.js
@@ -1,4 +1,4 @@
-let selector = document.getElementById('int');
+/*let selector = document.getElementById('int');
 setInterval(() => {
     switch (Math.floor(Math.random() * 6)) {
         case 0:
@@ -21,7 +21,7 @@ setInterval(() => {
             break;
     }
 }, 1000);
-
+*/
 
 let text = ['평범한 학생','중학생', '초보', '예비', '코딩을 좋아하는 학생', '어린'];
 function callMe(){
@@ -29,4 +29,4 @@ function callMe(){
   let curIdx = text.indexOf(myText.innerHTML);
   myText.innerHTML = text[(curIdx+1)%text.length];
 }
-setInterval(callMe,1300);
+setInterval(callMe,1500);
