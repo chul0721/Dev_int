0156d224ddcd25a6b484adc86eec1db03c027b33
int <64084503+chul0721@users.noreply.github.com>
2020-11-03 09:21:19 +0900

    Update index.js

--- public/index.js
+++ public/index.js
@@ -23,10 +23,16 @@ setInterval(() => {
 }, 1000);
 */
 
-let text = ['평범한 학생','중학생', '초보', '예비', '코딩을 좋아하는 학생', '어린'];
+let text = ['학생','초보', '예비', '코딩을 좋아하는'];
 function callMe(){
   let myText = document.getElementById('mytext');
   let curIdx = text.indexOf(myText.innerHTML);
   myText.innerHTML = text[(curIdx+1)%text.length];
 }
 setInterval(callMe,1500);
+    $(document).ready(function(){
+      $('#scroll').click(function(){
+      var offset = $('#page2').offset();
+        $('html').animate({scrollTop : offset.top}, 400);
+      });
+});
